2ca377914b433bac1eb7b62ee685a83db0a043b0
xckai <xckai123@gmail.com>
2023-03-21 23:07:53 +0800

    fix: add tracker

--- main.ts
+++ main.ts
@@ -1,31 +1,64 @@
-import { Application,Router } from "https://deno.land/x/oak/mod.ts";
-import {fetchAllProxy, getSubscribeDetail} from "./clashSubscribe.ts"
+import { Application, Router } from "https://deno.land/x/oak/mod.ts";
+import {
+  fetchAllProxy,
+  getSubscribeDetail,
+  sendTelegramMessage,
+} from "./clashSubscribe.ts";
 import "https://deno.land/x/dotenv/load.ts";
 
 const port = Deno.env.get("PORT") ?? "80";
-console.log(port,Deno.env.get("subscribeURL"))
+console.log(port, Deno.env.get("subscribeURL"));
 const router = new Router();
 
-router.get("/private", async ctx=>{
-  ctx.response.body = await getSubscribeDetail(ctx.request)
-})
-router.get("/proxy", async ctx=>{
-  ctx.response.body = await getSubscribeDetail(ctx.request)
-})
-router.get("/proxy/:proxy", async ctx=>{
-  ctx.response.body = await getSubscribeDetail(ctx.request)
-})
-router.get("/",  ctx=>{
-  ctx.response.body = "hello world"
-})
-router.get("/test",  async ctx=>{
-  ctx.response.body = await fetchAllProxy()
-})
+// router.get("/private", async ctx=>{
+//   ctx.response.body = await getSubscribeDetail(ctx.request)
+// })
+// router.get("/proxy", async ctx=>{
+//   ctx.response.body = await getSubscribeDetail(ctx.request)
+// })
+router.get("/proxy/:proxy", async (ctx) => {
+  const params = ctx.params;
+  const proxyName = params.proxy ?? "";
+  if (
+    ["mac", "encoohp", "openwrt"].indexOf(proxyName) != -1 ||
+    proxyName.indexOf("kai") != -1 ||
+    proxyName.indexOf("yh") != -1
+  ) {
+    ctx.response.body = await getSubscribeDetail(ctx.request);
+  }
+  sendTelegramMessage(`${new Date().toLocaleString("zh-CN", {
+    timeStyle: "medium",
+  })}
+  SourceIP: ${ctx.request.ip}
+  SourceReqPath: ${ctx.request.url.pathname}
+  未知源访问！`);
+  ctx.response.status = 404;
+});
+router.get("/", (ctx) => {
+  sendTelegramMessage(`${new Date().toLocaleString("zh-CN", {
+    timeStyle: "medium",
+  })}
+  SourceIP: ${ctx.request.ip}
+  SourceReqPath: ${ctx.request.url.pathname}
+  未知源访问！`);
+  ctx.response.body = "hello world";
+});
+
 const app = new Application();
 app.use(router.routes());
-
-console.log(`App listening on port ${port}, ${new Date().toLocaleString(
-  "zh-CN",
-  { dateStyle: "long", timeStyle: "medium" }
-)}`)
-await app.listen({ port:parseInt(port) })
+app.use((ctx)=>{
+  sendTelegramMessage(`${new Date().toLocaleString("zh-CN", {
+    timeStyle: "medium",
+  })}
+  SourceIP: ${ctx.request.ip}
+  SourceReqPath: ${ctx.request.url.pathname}
+  未知源访问！`);
+  ctx.response.status = 404;
+})
+console.log(
+  `App listening on port ${port}, ${new Date().toLocaleString("zh-CN", {
+    dateStyle: "long",
+    timeStyle: "medium",
+  })}`
+);
+await app.listen({ port: parseInt(port) });
